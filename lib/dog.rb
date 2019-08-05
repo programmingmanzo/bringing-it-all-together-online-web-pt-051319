9fe8878a9f08dd0f52112c58ab648fd4618cf470
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-05 02:54:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,6 @@
   attr_accessor :id, :name, :breed
 
   def initialize(attributes)
-    #id: nil, name:, breed:
     attributes.each {|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
   end
